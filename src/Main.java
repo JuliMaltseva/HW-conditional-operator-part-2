c0ff96368e49b941c97fc534558fbf4e2cbe8948
Юлия <maltssevaulia@gmail.com>
2022-11-23 23:45:50 +0300

    задание 1-2

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
 
         //Задание 3
         System.out.println("Задание 3");
-        int year = 2000;
+        int year = 2100;
         if (year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else
@@ -42,5 +42,50 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
 
-    }
+        //Задание 4
+        System.out.println("Задание 4");
+        int deliveryDistance = 102;
+        int oneDay = 20;
+        int day = 1;
+        int plusDay = 40;
+        int deliveryTime = (deliveryDistance - oneDay) / plusDay ;
+        //System.out.println(deliveryTime);
+        if (deliveryDistance <= oneDay) {
+            System.out.println("Для доставки потребуется " + day + " дней.");
+        } else
+            if ((deliveryDistance - oneDay) % plusDay == 0) {
+                System.out.println("Для доставки потребуется " + (day +deliveryTime) + " дней.");
+            } else {
+                System.out.println("Для доставки потребуется " + (day +deliveryTime + 1) + " дней.");
+            }
+
+        //Задание 5
+        System.out.println("Задание 5");
+            int monthNumber = 6;
+            switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Это зимний месяц");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Это весенний месяц");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Это летний месяц");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Это осенний месяц");
+                break;
+            default:
+                    System.out.println("Такого месяца не существует");
+            }
+
+        }
 }
